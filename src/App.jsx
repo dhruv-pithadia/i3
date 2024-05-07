083a8c2b965b5c4b2c56b2a7b084f788fa50be66
d-h-r-u-v-1-1 <dhruvpithadia11@gmail.com>
2024-05-07 11:53:22 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-main">
+      <div className="sub-main">
+        <h2>I3</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
