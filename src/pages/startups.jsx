286cf33d36d90d2f97c339d02c31ebea8360ae8e
d-h-r-u-v-1-1 <dhruvpithadia11@gmail.com>
2024-07-12 16:57:02 +0530

    startups

--- src/pages/startups.jsx
+++ src/pages/startups.jsx
@@ -1,19 +1,34 @@
 import React from 'react';
 import Header from './header';
 import Footer from './footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../static/startups.css';
-
+import { Container, Row, Col } from 'react-bootstrap';
 
 const Startups = () => {
 
     return (
-        <div className="startup-main">
-            <Header />
-            <div className="startup-sub-main">
+        <main>
+            <div className="startup-main">
+                <Header />
+                <div className="startup-sub-main">
 
+
+                    <Row className='startups text-center'>
+                        <Col>
+                            <div className="banner d-flex justify-content-center align-items-center">
+                                <div className="banner-text">
+                                    <h1>Startups</h1>
+                                </div>
+                            </div>
+                        </Col>
+                    </Row>
+
+
+                </div>
+                <Footer />
             </div>
-            <Footer />
-        </div>
+        </main>
     );
 }
 
